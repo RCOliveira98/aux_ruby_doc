f108bccba265c2c10d585e6d15050426f71030f2
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-05 20:59:33 -0300

    first-steps: update

--- src/app/modules/first-steps/first-steps.module.ts
+++ src/app/modules/first-steps/first-steps.module.ts
@@ -12,15 +12,17 @@ import { MacOsxComponent } from './setup/installation/mac-osx/mac-osx.component'
 import { InputOutputComponent } from './input-output/input-output.component';
 import { HelloWordComponent } from './input-output/hello-word/hello-word.component';
 import { AuxToolsComponent } from './setup/installation/aux-tools/aux-tools.component';
+import { IntroToolsComponent } from './setup/installation/aux-tools/intro-tools/intro-tools.component';
+import { ImageToolsComponent } from './setup/installation/aux-tools/image-tools/image-tools.component';
+import { IstallToolsComponent } from './setup/installation/aux-tools/istall-tools/istall-tools.component';
+import { IdeComponent } from './setup/installation/aux-tools/ide/ide.component';
 
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { IntroToolsComponent } from './setup/installation/aux-tools/intro-tools/intro-tools.component';
-import { ImageToolsComponent } from './setup/installation/aux-tools/image-tools/image-tools.component';
-import { IstallToolsComponent } from './setup/installation/aux-tools/istall-tools/istall-tools.component';
+import { NzListModule } from 'ng-zorro-antd/list';
 
 
 
@@ -29,7 +31,8 @@ const ngZorroAnt = [
   NzCollapseModule,
   NzSelectModule,
   NzAlertModule,
-  NzCardModule
+  NzCardModule,
+  NzListModule
 ];
 
 @NgModule({
@@ -45,7 +48,8 @@ const ngZorroAnt = [
     AuxToolsComponent,
     IntroToolsComponent,
     ImageToolsComponent,
-    IstallToolsComponent
+    IstallToolsComponent,
+    IdeComponent
   ],
   imports: [
     CommonModule,
