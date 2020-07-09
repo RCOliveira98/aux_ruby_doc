a9a2b1669a08f57b0545bc9796768b36d562e848
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-09 18:56:33 -0300

    [first-steps] create data-types component

---
+++ src/app/modules/first-steps/data-types/string-type/string-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-type',
+  templateUrl: './string-type.component.html',
+  styleUrls: ['./string-type.component.css']
+})
+export class StringTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
