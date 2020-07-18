b901ebabb5561719a95b9d5e6533b9d03abb0469
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-18 16:34:33 -0300

    [first-steps] data-types: update content

--- src/app/modules/first-steps/data-types/data-types.component.ts
+++ src/app/modules/first-steps/data-types/data-types.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTypesComponent implements OnInit {
 
+  troubledComputer: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.troubledComputer = 'https://i.ibb.co/23byLZZ/computer-Off.png';
   }
 
 }
