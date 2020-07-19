423e84cfda22afc1938f20d118666afd5b89f167
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-07-19 18:31:02 -0300

    [first-steps] create component snake-case

---
+++ src/app/modules/first-steps/input-output/reader/snake-case/snake-case.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snake-case',
+  templateUrl: './snake-case.component.html',
+  styleUrls: ['./snake-case.component.css']
+})
+export class SnakeCaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
